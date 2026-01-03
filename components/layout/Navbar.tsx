88b042f8a26113f39fc2bc02f7958a6778ddaffd
kira-1011 <kirubel.s.tadesse@gmail.com>
2026-01-03 19:32:12 +0300

    refac

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -20,7 +20,8 @@ interface NavLinkProps {
 }
 
 function NavLink({ href, children, className, onClick }: NavLinkProps) {
-	const baseStyles = "relative overflow-hidden py-1 font-semibold uppercase text-xs tracking-[0.25em]";
+	const baseStyles =
+		"relative overflow-hidden py-1 font-semibold uppercase text-xs tracking-[0.25em]";
 
 	const isHashLink = href.startsWith("#");
 
