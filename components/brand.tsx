8f3f998f0e0b50388d1a4ca1b3b83c56bf05d515
kira-1011 <kirubel.s.tadesse@gmail.com>
2026-01-04 11:45:30 +0300

    brand update

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,6 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 import logo from "@/public/logo.svg";
 
 type BrandVariant = "logo" | "logo-with-text" | "full";
